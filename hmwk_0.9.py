54c61d52a0819b0b8600c9e30296d32101b7a64b
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-09-22 18:14:28 +0300

    Add files via upload

---
+++ hmwk_0.9.py
@@ -0,0 +1,9 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in other_words:
+        if i.lower() in root_word.lower() or root_word.lower() in i.lower():
+            same_words.append(i)
+    return same_words
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
